b5e7932b2ab123ada9004c73a31bf230414847da
saurav <saurav@issac.(none)>
2014-01-05 21:39:34 +0530

    optimized some code

--- app.py
+++ app.py
@@ -1,30 +1,16 @@
 #!/usr/bin/env python
-
-import urllib
-
-import logging
-import json
-import urllib2
 from bs4 import BeautifulSoup
-
-import sys
-import webapp2
-
 from local_settings import username,password
-
 import twill
 from twill.commands import *
 
 class Bot(webapp2.RequestHandler):
     def get(self):
-        #self.response.out.write(self.gen_post())
         go('http://hackerstreet.in/submit')
-        #showforms()
         fv("1", "u", username)
         fv("1", "p", password)
         submit('0')
         go('http://hackerstreet.in/submit')
-        #showforms()
         text,url = self.gen_post()
         fv("1", "t", text)
         fv("1", "u", url)
@@ -33,13 +19,11 @@ class Bot(webapp2.RequestHandler):
     def gen_post(self):
         url = "https://twitter.com/yugdom"
         soup = BeautifulSoup( urllib2.urlopen(url).read() )
-
         #finding the first tweet in yugdom twitter handle
         tweet = soup.find('p',{'class':'js-tweet-text'}).get_text()
-
         #removing non-ascii characters form tweet
         tweet = "".join(i for i in tweet if ord(i)<128)
-        
+        #splitting tweet into text and url
         text = tweet.split(' ')[:-1]
         text = ' '.join(text)
         url = tweet.split(' ')[-1]
